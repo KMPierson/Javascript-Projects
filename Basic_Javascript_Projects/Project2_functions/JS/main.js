548f174b761e9e033ddfb7d959e95575fa4bcad2
Kiera Pierson <kieramarie_32@hotmail.com>
2022-03-10 09:46:21 -0500

    Project 2

---
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function my_first_function(){  //This defines the funtion
+    var str="This is the ";
+        str +="button text!";  //This is how to use the += method to concatenate an element
+    document.getElementById("button_text").innerHTML=str;
+}
+
