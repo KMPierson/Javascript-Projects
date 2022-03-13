07bd5148a9ebbcd5cb41b96b5769792f6be6af19
Kiera Pierson <kieramarie_32@hotmail.com>
2022-03-13 18:28:25 -0400

    slideshow

---
+++ Basic_Javascript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,44 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        TimeRanges.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            TimeRanges.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//next previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", " ");
+    }    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
