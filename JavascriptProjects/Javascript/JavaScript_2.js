cf2d8a48a2cd3ad7f1aa10047df789f970ecb6e7
Kiera Pierson <99521252+KMPierson@users.noreply.github.com>
2022-05-15 13:55:25 -0400

    Delete JavascriptProjects directory

--- JavascriptProjects/Javascript/JavaScript_2.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    let x = document.forms["myForm"] ["email"].value;
-    if (x== "") {
-        alert("Email must be filled out");
-        return false;
-    }
-}
--- JavascriptProjects/Javascript/JavaScript_3.js
+++
@@ -1,4 +0,0 @@
-function displayType(character) {
-    var characterType = character.getAttribute("data-character-type");
-    alert(characterType + " is in the " + character.innerHTML + " Universe!");
-}
